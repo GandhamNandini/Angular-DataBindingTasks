6b5a539b73fe295a6760b5f2fee79cf796f069d6
GandhamNandini <nandininadu23@gmail.com>
2019-08-08 10:26:46 +0530

    Html-to-Console

---
+++ src/app/component1/component1.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component1',
+  templateUrl: './component1.component.html',
+  styles: []
+})
+export class Component1Component implements OnInit {
+
+  logMessage(value){
+    console.log(value);
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
